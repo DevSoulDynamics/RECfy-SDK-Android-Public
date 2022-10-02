b33270df97489f28b2509d0de21fdc74ca1c085e
Jorge Guerrero Peralta <jguerreropedev@gmail.com>
2022-10-02 22:19:07 +0200

    create public sdk

---
+++ README.md
@@ -0,0 +1,35 @@
+# Recfy SDK
+
+## Installation
+
+root project gradle
+
+```Gradle 
+maven {
+    name = "GitHubPackages"
+    url = uri("https://maven.pkg.github.com/DevSoulDynamics/RECfy-SDK-Android")
+    credentials {
+        username 'GITHUB_username'
+        password 'GITHUB_token'
+    }
+}
+```
+project gradle
+```Gradle 
+implementation "com.devsoul.recfy:sdk:VERSION"
+```
+
+## Usage
+initialization
+```kotlin
+class MainActivity : AppCompatActivity() {
+    private val uploaderSDK: RecfyUploaderSDK by lazy {
+        RecfyUploaderSDK.create(this)
+    }
+}
+```
+
+Upload Media
+```kotlin
+uploaderSDK.uploadMedia(USER_EMAIL, EVENT_CODE, USER_GDPR)
+```
---
+++ uploader-sdk-example/src/main/java/com/devsoul/recfy/sdk/example/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.devsoul.recfy.sdk.example
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.devsoul.recfy.sdk.RecfyUploaderSDK
+import com.devsoul.recfy.uploadersdk.example.BuildConfig
+import com.devsoul.recfy.uploadersdk.example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val uploaderSDK: RecfyUploaderSDK by lazy {
+        RecfyUploaderSDK.create(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUp()
+    }
+
+    private fun setUp() {
+        if (BuildConfig.DEBUG) {
+            binding.emailEditText.setText(USER_EMAIL)
+            binding.eventEditText.setText(EVENT_CODE)
+        }
+
+        binding.uploadButton.setOnClickListener {
+            checkForm()
+        }
+    }
+
+    private fun checkForm() {
+        val email = binding.emailEditText.text.toString()
+        val eventCode = binding.eventEditText.text.toString()
+
+        if(email.isBlank()){
+            toastMessage("email invalid")
+            return
+        }
+
+        if(eventCode.isBlank()){
+            toastMessage("eventCode invalid")
+            return
+        }
+
+        uploaderSDK.uploadMedia(email, eventCode, USER_GDPR)
+    }
+
+    private fun toastMessage(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        const val EVENT_CODE = "Devsoul"
+        const val USER_EMAIL = "test@test.com"
+        const val USER_GDPR = true
+    }
+}
