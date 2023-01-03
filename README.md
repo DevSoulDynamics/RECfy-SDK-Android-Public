822bfc9f4a7125b1896635cd465a80f3564008d6
Jorge Guerrero Peralta <jguerreropedev@gmail.com>
2023-01-03 14:04:25 +0100

    update sdk version to 0.2.0
    * add example to handle upload process result by activityRessult

--- README.md
+++ README.md
@@ -33,6 +33,22 @@ Upload Media
 ```kotlin
 uploaderSDK.uploadMedia(USER_EMAIL, EVENT_CODE, USER_GDPR)
 ```
+The upload process result can handle by overriding the activity result
+```kotlin
+override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    when (requestCode) {
+        RecfyUploaderSDK.ACTION_UPLOAD  -> {
+            when (resultCode) {
+                Activity.RESULT_OK -> "media uploaded."
+                RecfyUploaderSDK.RESULT_UPLOAD_FAIL -> "media upload fail."
+                RecfyUploaderSDK.RESULT_USER_CANCEL -> "media upload cancel by user"
+            }
+        }
+    }
+}
+```
 
 ## Example
 Simple app to test the SDK, to run the example you should create a `github.properties` file:
--- uploader-sdk-example/src/main/java/com/devsoul/recfy/sdk/example/MainActivity.kt
+++ uploader-sdk-example/src/main/java/com/devsoul/recfy/sdk/example/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.devsoul.recfy.sdk.example
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.devsoul.recfy.sdk.RecfyUploaderSDK
-import com.devsoul.recfy.sdk.example.BuildConfig
 import com.devsoul.recfy.sdk.example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setUp()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            RecfyUploaderSDK.ACTION_UPLOAD  -> {
+                when (resultCode) {
+                    Activity.RESULT_OK ->  toastMessage("media uploaded.")
+                    RecfyUploaderSDK.RESULT_UPLOAD_FAIL ->  toastMessage("media upload fail.")
+                    RecfyUploaderSDK.RESULT_USER_CANCEL ->  toastMessage("media upload cancel by user")
+                }
+            }
+            else -> {
+                // nothing to do
+            }
+        }
     }
 
     private fun setUp() {
